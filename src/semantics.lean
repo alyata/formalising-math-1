a9774ac376030bba915c39109abaf288cc2c3932
ar2519 <ar2519@ic.ac.uk>
2022-02-01 11:34:03 +0000

    shortened the semantic proofs

--- src/semantics.lean
+++ src/semantics.lean
@@ -6,7 +6,7 @@ variable {vars : Type}
     truth assignment
 -/
 def eval (v : vars → bool) : Form vars → bool
-| ⊥       := false
+| ⊥       := ff
 | ⦃x⦄     := v x
 | (~ P)   := not (eval P)
 | (P ⋀ Q) := and (eval P) (eval Q)
@@ -25,9 +25,9 @@ def entail (Γ : set (Form vars)) (A : Form vars) : Prop :=
 notation Γ ` ⊨ ` A := entail Γ A
 notation Γ ` ⊭ ` A := ¬ entail Γ A
 
-@[simp] def satisfiable (Γ : set (Form vars)) : Prop :=
+def satisfiable (Γ : set (Form vars)) : Prop :=
 ∃ (v : vars → bool), v ⊨ Γ
-@[simp] def unsatisfiable (Γ : set (Form vars)) : Prop :=
+def unsatisfiable (Γ : set (Form vars)) : Prop :=
 ¬ satisfiable Γ
 
 variable {Γ : set (Form vars)}
@@ -37,25 +37,7 @@ by simp [satisfiable, entail, eval]
 
 theorem unsatisfiable_iff : unsatisfiable Γ ↔ (Γ ⊨ ⊥) :=
 begin
-  simp [eval, entail],
-  split,
-  {
-    intros h v hΓ,
-    rcases h v with ⟨γ, hin, hγ⟩,
-    specialize hΓ γ hin,
-    simp [hγ] at hΓ,
-    exact hΓ
-  },
-  {
-    intros h v,
-    specialize h v,
-    by_contra h',
-    simp at h',
-    apply h,
-    intros γ hin,
-    specialize h' γ hin,
-    simp [h'],
-  }
+  simp [unsatisfiable, satisfiable_iff],
 end
 
 #lint
